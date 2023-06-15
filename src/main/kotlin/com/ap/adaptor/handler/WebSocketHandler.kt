d10df3618461219afb0b47de89f2af1e7fe6f5bd
tmdrl5779 <tmdrl5779@gmail.com>
2023-06-15 21:59:02 +0900

    [update] websocket 핸들러 수정

--- src/main/kotlin/com/ap/adaptor/handler/WebSocketHandler.kt
+++ src/main/kotlin/com/ap/adaptor/handler/WebSocketHandler.kt
@@ -15,53 +15,12 @@ class WebSocketHandler(
     val adaptorService: AdaptorService
 ) : WebSocketHandler {
 
-//
-//    override fun afterConnectionEstablished(session: WebSocketSession) {
-//        // WebSocket 연결이 수립되었을 때 실행되는 메서드
-//    }
-//
-//    override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
-//        // WebSocket 연결이 종료될 때 실행되는 메서드
-//    }
-//
-//    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) = runBlocking{
-//        val payload = message.payload
-//
-//        // payload 파싱하여 사용자 수, 반복 간격, 반복 횟수 등을 추출합니다.
-//        val userCount = parseUserCount(payload)
-//        val interval = parseInterval(payload)
-//        val repeatCount = parseRepeatCount(payload)
-//
-//
-//        val coroutineScope = CoroutineScope(Dispatchers.IO)
-//
-//        val job = coroutineScope.launch {
-//
-//
-//            delay(interval)
-//        }
-//
-//        job.join()
-//        session.close()
-//    }
-
-//    override fun handle(webSocketSession: WebSocketSession): Mono<Void> {
-//        val stringFlux = webSocketSession.receive()
-//            .map { obj: WebSocketMessage -> obj.payloadAsText }
-//            .map { obj: String -> obj.toUpperCase() }
-//            .map { payload: String? ->
-//                webSocketSession.textMessage(
-//                    payload!!
-//                )
-//            }
-//        return webSocketSession.send(stringFlux)
-//    }
-
     override fun handle(session: WebSocketSession): Mono<Void> {
 
-        val inbound = session.receive()
-            .map { message -> message.payloadAsText }
-            .flatMap { payload ->
+        val receiveAndCallApi = session.receive()
+            .flatMap { message ->
+                val requestData = message.payloadAsText
+
 //                val userCount = parseUserCount(payload)
 //                val interval = parseInterval(payload)
                 val interval = 1L
@@ -71,20 +30,39 @@ class WebSocketHandler(
                 val coroutineScope = CoroutineScope(Dispatchers.IO)
                 coroutineScope.launch {
                     repeat(repeatCount) {
-                        session.send(Mono.just(session.textMessage("aaaaaaaaaaaaaaa"))).subscribe()
+
+                        val response = callApi(requestData)
+
+                        session.send(Mono.just(session.textMessage(response))).subscribe()
                         delay(interval)
                     }
-                    session.send(Mono.just(session.textMessage("connection close"))).then(session.close()).subscribe()
+                    session.send(Mono.just(session.textMessage("connection close success"))).then(session.close()).subscribe()
 
                 }
                 Mono.empty<Void>()
 
             }
+            .doOnError { error ->
+                session.send(Mono.just(session.textMessage("connection close : $error"))).then(session.close()).subscribe()
+            }
             .then()
 
-        return inbound
+
+        return session.send(Mono.just(session.textMessage("Connection open success")))
+            .thenMany(receiveAndCallApi)
+            .then()
+
+
+//        return receiveAndCallApi
 
     }
+
+
+    private suspend fun callApi(requestData: String): String {
+        return requestData
+    }
+
+
 
     private fun parseRepeatCount(payload: String): Int {
         TODO("Not yet implemented")
