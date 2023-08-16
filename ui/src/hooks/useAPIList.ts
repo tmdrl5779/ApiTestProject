81984029de32304ca75f2710dac708ee17122626
82240943 <hyunwoo.jeong@kt.com>
2023-08-16 14:38:13 +0900

    [update] APIs 상태관리 구조 잡는중-2

--- ui/src/hooks/useAPIList.ts
+++ ui/src/hooks/useAPIList.ts
@@ -1,6 +1,13 @@
 import { APIListState } from '@/data/store'
+import { useCallback } from 'react'
 import { useRecoilState } from 'recoil'
 
 export const useAPIList = () => {
   const [APIList, setAPIList] = useRecoilState(APIListState)
+
+  const createAPI = useCallback(() => {}, [])
+
+  const deleteAPI = useCallback((idx: number) => {}, [])
+
+  const updateAPI = useCallback((idx: number) => {}, [])
 }
