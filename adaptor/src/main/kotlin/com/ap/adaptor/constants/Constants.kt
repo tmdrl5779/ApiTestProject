3759cdb212ec1eb20eae7da1e015742991fb2689
tmdrl5779 <tmdrl5779@gmail.com>
2023-12-15 20:56:31 +0900

    [update] Method 상수 변경, @정현우 api url이 /call/api가 아니라 /api/call임 확인점~

--- adaptor/src/main/kotlin/com/ap/adaptor/constants/Constants.kt
+++ adaptor/src/main/kotlin/com/ap/adaptor/constants/Constants.kt
@@ -1,9 +1,9 @@
 package com.ap.adaptor.constants
 
 object Constants {
-    const val HTTP_METHOD_GET = "get"
-    const val HTTP_METHOD_POST = "post"
-    const val HTTP_METHOD_PUT = "put"
-    const val HTTP_METHOD_DELETE = "delete"
-    const val HTTP_METHOD_PATCH = "patch"
+    const val HTTP_METHOD_GET = "GET"
+    const val HTTP_METHOD_POST = "POST"
+    const val HTTP_METHOD_PUT = "PUT"
+    const val HTTP_METHOD_DELETE = "DELETE"
+    const val HTTP_METHOD_PATCH = "PATCH"
 }
