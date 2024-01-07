b3ca3dd839f026300b7a9bb34386341fec8728a5
tmdrl5779 <tmdrl5779@gmail.com>
2024-01-07 15:06:06 +0900

    [update] response data api 응답값 header, cookie 추가

--- adaptor/src/main/kotlin/com/ap/adaptor/dto/ResponseData.kt
+++ adaptor/src/main/kotlin/com/ap/adaptor/dto/ResponseData.kt
@@ -3,5 +3,7 @@ package com.ap.adaptor.dto
 data class ResponseData(
     var responseTime: Long = 0,
     var body: Any ?= null,
-    var status: String = ""
+    var status: String = "",
+    var header: Any ?= null,
+    var cookie: Any ? = null,
 )
