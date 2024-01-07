81e8d117b88614bdffb1b67648a1f0280024214c
tmdrl5779 <tmdrl5779@gmail.com>
2024-01-07 17:46:36 +0900

    [update] webcleint 임시 수정

--- adaptor/src/main/kotlin/com/ap/adaptor/config/WebClientConfig.kt
+++ adaptor/src/main/kotlin/com/ap/adaptor/config/WebClientConfig.kt
@@ -6,21 +6,28 @@ import io.netty.handler.timeout.WriteTimeoutHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
-import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient
+import reactor.netty.resources.ConnectionProvider
+
 
 @Configuration
 class WebClientConfig {
 
     @Bean
     fun webClient(): WebClient {
-        val httpClient = HttpClient.create()
+        val connectionProvider =
+            ConnectionProvider.builder("myConnectionPool").maxConnections(10000).pendingAcquireMaxCount(10000).build()
+
+        val httpClient = HttpClient
+            .create(connectionProvider)
             .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 100000)
-            .doOnConnected{ conn -> conn
-                .addHandlerLast(ReadTimeoutHandler(300))
-                .addHandlerLast(WriteTimeoutHandler(300))
+            .doOnConnected { conn ->
+                conn
+                    .addHandlerLast(ReadTimeoutHandler(300))
+                    .addHandlerLast(WriteTimeoutHandler(300))
             }
+
 
         return WebClient.builder()
             .clientConnector(ReactorClientHttpConnector(httpClient))
