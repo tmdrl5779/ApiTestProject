41b37f4583cec0ab52b34473c8bc8abb3fbb342b
hyunwoo <jhw1234527@gmail.com>
2024-01-21 16:10:44 +0900

    [update] 로컬스토리지에 api, 테스트 데이터 저장

--- ui/src/data/store.ts
+++ ui/src/data/store.ts
@@ -1,10 +1,24 @@
 import { getDefaultAPI } from './constants'
 import { IAPI } from 'api-types'
-import { atom } from 'recoil'
+import { atom, AtomEffect } from 'recoil'
+
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  key =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key)
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue))
+    }
+
+    onSet((newValue, _, isReset) => {
+      isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue))
+    })
+  }
 
 export const APIListState = atom<IAPI[]>({
   key: 'apiList',
   default: [getDefaultAPI()],
+  effects: [localStorageEffect<IAPI[]>('apiList')]
 })
 
 const defaultAPIForTest = getDefaultAPI()
@@ -13,6 +27,7 @@ defaultAPIForTest.request.url = 'https://jsonplaceholder.typicode.com/todos/1'
 export const APIListForTestState = atom<IAPI[]>({
   key: 'apiListForTest',
   default: [defaultAPIForTest],
+  effects: [localStorageEffect<IAPI[]>('apiListForTest')]
 })
 
 export interface TestMetaData {
@@ -32,4 +47,5 @@ const defaultTestMetaData: TestMetaData = {
 export const MetaDataForTestState = atom<TestMetaData>({
   key: 'metaDataForTest',
   default: defaultTestMetaData,
+  effects: [localStorageEffect<TestMetaData>('metaDataForTest')]
 })
